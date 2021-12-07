79eeef1fa5dc57c192bbff40e19011f5d7b15db8
Osarodion Irabor <33585132+lightning-dabbler@users.noreply.github.com>
2021-12-06 21:10:39 -0500

    day2 completed

---
+++ solutions/day2.py
@@ -0,0 +1,32 @@
+import sys
+
+def part1():
+    horizontal = 0
+    depth = 0
+    for line in sys.stdin:
+        direction,units = line.strip().split(' ')
+        units = int(units)
+        if direction == "forward":
+            horizontal+=units
+        elif direction == "down":
+            depth += units
+        else:
+            depth-=units
+    return horizontal*depth
+
+def part2():
+    horizontal = 0
+    depth = 0
+    aim = 0
+    for line in sys.stdin:
+        direction,units = line.strip().split(' ')
+        units = int(units)
+        if direction == "forward":
+            horizontal+=units
+            depth+=(units*aim)
+        elif direction == "down":
+            aim += units
+        else:
+            aim-=units
+    return horizontal*depth
+        
